1c1fb9c58fbf603517a4ee327ef9c2b9658a967b
deviengeorge <deviengeorge99@gmail.com>
2022-02-01 23:08:31 +0200

    adding material for lec3

--- components/Greeting.js
+++ components/Greeting.js
@@ -6,31 +6,8 @@
 const Greeting = {
 	props: {
 		name: String,
-		day: {
-			type: [Number, String],
-		},
-		year: {
-			type: Number,
-			required: true,
-		},
-		month: {
-			type: Number,
-			default: function () {
-				return 5;
-			},
-			validator: function (value) {
-				if (value >= 1 && value <= 12) {
-					return 1;
-				} else {
-					return 0;
-				}
-			},
-		},
 	},
 	template: `
     <p>name: {{ name }}</p>
-    <p>month: {{ month }}</p>
-    <p>year: {{ year }}</p>
-    <p>day: {{ day }}</p>
   `,
 };
--- index.js
+++ index.js
@@ -1,31 +1,80 @@
-const Card = {
-	template: `<article>
-		<p>card</p>
-	</article>`,
-};
+// v-if,
+// v-else-if,
+// v-else,
+// v-bind,
+// V-show
+// v-for
+// v-html
+// v-text
+
+// Not Completed v-on
 
 const app = Vue.createApp({
+	// Private
 	components: {
 		Header: Header,
 		Body: Body,
 		Greeting: Greeting,
-		Card: Card,
+	},
+	data: function () {
+		return {
+			arr: ['ahmed', 'ahmed', 'ahmed', 'ahmed', 'ahmed', 'ahmed', 'ahmed'],
+			person: {
+				age: 20,
+				name: 'Ahmed Khaled',
+				address: 'Cairo',
+			},
+			imgSrc: 'https://via.placeholder.com/350x150',
+			altValue: 'hello Image123',
+			idValue: 'myImg',
+		};
 	},
 	template: `
 		<div>
-			<Header />
-			<h1>Index Page</h1>
+			<Header  />
+
+			<button @click="arr[2] = 'mohamed'">Click ME</button>
+
+			<p v-text="'<h1>hello</h1>'"></p>
+
+			<p v-html="'<h1>This IS My Wesbite</h1>'"></p>
+
+
 			<Greeting
-				name="654544"
-				v-bind:month="50"
-				v-bind:year="2001"
-				day="Sunday" />
+				v-bind:name="person.name"
+			/>
+
+			
+
 			<Body></Body>
 		</div>
 	`,
 });
 
+// arr = ["1", "2", "3"]
+// for (const item in arr) {
+
+// }
+
 // 654544
 // "654544"
 
 app.mount('#app');
+
+// V-If
+// <h1 v-if="age > 18">You Are UnderAge</h1>
+// <h1 v-else-if="name === 'Admin'">You Are Admin</h1>
+// <h1 v-else>Else</h1>
+
+// V-For
+// <p v-for="(myItem, i) in arr">{{ myItem }} - {{ i }}</p>
+// <p v-for="(value, key1, index) in person">{{ key1 }}: {{ value }} - {{ index }}</p>
+
+// V-Bind
+// <img v-bind="{ id: 'hello img id', alt: altValue }" :title="myTitle" :src="imgSrc" />
+
+// V-show
+// <p v-show="false">Hello World With Show</p>
+
+// .innerText = '<p>hello</p>'
+// .innerHTML = '<p>hello</p>'
